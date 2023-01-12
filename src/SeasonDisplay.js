d54f4ef5e70cff53db3314f78597f44b9fe30b52
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-12 12:46:52 +0530

    Add logic to detect the season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,18 @@
+const getSeason = (lat, month) => {
+  // For north hemisphere, lat > 0
+  // For south hemisphere, lat < 0
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
 const SeasonDisplay = ({ lat }) => {
+  const season = getSeason(lat, new Date().getMonth());
+
+  console.log(season);
+
   return <div>Latitude: {lat}</div>;
 };
 
